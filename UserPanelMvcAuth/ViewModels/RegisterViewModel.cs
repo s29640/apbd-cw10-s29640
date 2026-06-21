917f928d85138b0d5f4c19d8ac81ea20c70ddb55
Robert Sendys <s29640@pjwstk.edu.pl>
2026-06-21 15:40:39 +0200

    Dodaj view models

---
+++ UserPanelMvcAuth/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserPanelMvcAuth.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(8)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+    }
+}
