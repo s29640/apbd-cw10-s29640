ebb596f1567e99fb9d550dbba924b3b1e6863449
Robert Sendys <s29640@pjwstk.edu.pl>
2026-06-21 15:20:32 +0200

    Utwórz modele

---
+++ UserPanelMvcAuth/Models/AppUser.cs
@@ -0,0 +1,12 @@
+﻿namespace UserPanelMvcAuth.Models
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public required string Email { get; set; }
+        public required string PasswordHash { get; set; }
+        public required string Role { get; set; } = "User";
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public ICollection<UserNote> Notes { get; set; } = new List<UserNote>();
+    }
+}
---
+++ UserPanelMvcAuth/Models/UserNote.cs
@@ -0,0 +1,12 @@
+﻿namespace UserPanelMvcAuth.Models
+{
+    public class UserNote
+    {
+        public int Id { get; set; }
+        public int AppUserId { get; set; }
+        public required string Title { get; set; }
+        public required string Content { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public AppUser AppUser { get; set; } = null!;
+    }
+}
